5bd2f0a50d04ce480b783ade70475fe37ebd028e
Kirill Dudnik <prog.me.2017@gmail.com>
2020-12-06 18:19:56 +0600

    Версия 1.0! - создал проект и настроил для Андроид; - добавил простое передвижение на основе анимации и сферы; - добавил слежение камеры за игроком; - начальные установки света;

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private Transform _player;
+    private Transform _transform;
+    private Vector3 _offset;
+
+	private void Start()
+    {
+        _transform = transform;
+        _offset = _transform.position - _player.position;
+
+    }
+
+    private void Update()
+    {
+        if(_player != null)
+        {
+            FollowThePlayer(_player);
+        }
+    }
+
+    private void FollowThePlayer(Transform player)
+	{
+		_transform.position = new Vector3(player.position.x + _offset.x, player.position.y + _offset.y, transform.position.z);
+
+        /*
+        var target = new Vector3(player.position.x + _offset.x, player.position.y + _offset.y, transform.position.z);
+        Vector3 currentPosition = transform.position;
+        transform.position = currentPosition;
+        */
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(Animator))]
+public class Player : MonoBehaviour
+{
+    private Rigidbody _rigidbody;
+    private Animator _animator;
+    private Transform _transform;
+    [SerializeField] private float _moveSpeed = 2f;
+
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _animator = GetComponent<Animator>();
+        _transform = transform;
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Debug.Log(_transform.position);
+        var _target = _transform.position + new Vector3(1f, 0f, 0f);
+        _transform.position = Vector3.Lerp(_transform.position, _target, Time.deltaTime * _moveSpeed);
+        Debug.Log(_transform.position);
+    }
+
+    private void StartMoveAnimation()
+    {
+
+    }
+}
